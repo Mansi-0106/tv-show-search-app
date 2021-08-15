5fa589688fa3015c0f1dcc8499116c6f9f209085
HEY !! <mansijoshi20000@gmail.com>
2021-08-16 00:46:35 +0530

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+var form = document.querySelector("#searchform");
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    var searchterm = form.elements.query.value;
+    var config = { params: { q: searchterm } }
+    var res = await axios.get(` https://api.tvmaze.com/search/shows`, config);
+    makeimage(res.data)
+    form.elements.query.value = '';
+
+})
+
+
+var makeimage = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+
+        }
+    }
+}
